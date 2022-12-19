a8eedeacb2bfaab098fe2ac808f556265beea88e
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2022-12-19 11:20:52 -0300

    atualização

--- 18_webpack/webpack.config.js
+++ 18_webpack/webpack.config.js
@@ -1,10 +1,10 @@
 const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-//const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-
+// const TerserPlugin = require('terser-webpack-plugin')
+ 
 module.exports = {
     mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
@@ -12,18 +12,20 @@ module.exports = {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
     optimization: {
-        minimizer: [            
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
             new OptimizeCSSAssetsPlugin({})
         ]
     },
     plugins: [
-        new TerserPlugin({
-            parallel: true,
-            terserOptions: {
-                ecma: 6,
-            },
-        }),
         new MiniCssExtractPlugin({
             filename: "estilo.css"
         })
@@ -33,7 +35,7 @@ module.exports = {
             test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
-                //'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
                 'css-loader', // interpreta @import, url()...
                 'sass-loader',
             ]
