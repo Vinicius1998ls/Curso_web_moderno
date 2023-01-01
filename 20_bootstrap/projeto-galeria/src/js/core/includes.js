3cdcdcf6984356808b14d56ef8f5623a9a5f3c5f
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-01-01 12:40:32 -0300

    Finalização do capitulo

--- 20_bootstrap/projeto-galeria/src/js/core/includes.js
+++ 20_bootstrap/projeto-galeria/src/js/core/includes.js
@@ -1,5 +1,13 @@
 import $ from 'jquery'
 
+const loadHTMLSuccessCallbacks = []
+
+export function onLoadHtmlSuccess(callback) {
+    if(!loadHTMLSuccessCallbacks.includes(callback)) {
+        loadHTMLSuccessCallbacks.push(callback)
+    }
+}
+
 function loadIncludes(parent) {
     if(!parent) parent = 'body'
     $(parent).find('[wm-include]').each(function(i, e) {
@@ -10,6 +18,7 @@ function loadIncludes(parent) {
                 $(e).html(data)
                 $(e).removeAttr('wm-include')
 
+                loadHTMLSuccessCallbacks.forEach(callback => callback(data))
                 loadIncludes(e)
             }
         })
