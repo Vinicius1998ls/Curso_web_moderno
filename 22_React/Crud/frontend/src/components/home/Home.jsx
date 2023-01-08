c09ac14f7bba8474231a040a12cc97f96e2e4073
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-01-08 13:02:05 -0300

    Atualização

---
+++ 22_React/Crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default proos =>
+    <Main icon="home" tittle="Início"
+        subtitle="Segundo Projeto do Capítulo de React.">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção
+            de um cadastro desenvolvido em React!</p>
+    </Main>
--- 22_React/Crud/frontend/src/components/template/Header.jsx
+++ 22_React/Crud/frontend/src/components/template/Header.jsx
@@ -2,6 +2,9 @@ import './Header.css'
 import React from 'react'
 
 export default props =>
-    <header className="header">
-        Cabeçalho
+    <header className="header d-name d-sm-flex flex-column">
+        <h1 className="mt-3">
+            <i className={`fa fa-${props.icon}`}></i> {props.tittle}
+        </h1>
+        <p className="leat text-muted">{props.subtitle}</p>
     </header>
