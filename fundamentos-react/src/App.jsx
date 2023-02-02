74b83213f62894439c762899a65eda5a2974a1c6
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-02-02 09:22:36 -0300

    Atualização

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css'
 import React from "react";
 
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import ComFilhos from './components/basicos/ComFilhos'
 import Card from './components/layout/Card'
-import Repeticao from './components/Repeticao';
-import Condicional from './components/Condicional';
-import CondicionalComIf from './components/CondicionalComIf';
+import Repeticao from './components/basicos/Repeticao';
+import Condicional from './components/basicos/Condicional';
+import CondicionalComIf from './components/basicos/CondicionalComIf';
 
 export default (props) => (
     <div className="App">
-        <Card titulo="#06 Condicional v2">
+        <Card titulo="#06 Condicional v2" color='#FA6900'>
             <CondicionalComIf numero={6}></CondicionalComIf>
         </Card>
         <Card titulo="#05 Condicional v1">
