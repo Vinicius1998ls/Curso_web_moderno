964d6b6deb70ef911964c5bed3af0104a64b0534
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-08-02 15:19:41 -0300

    Fim seção 29 projeto montyhall(vue)

--- 26_vueJS/montyhall/src/App.vue
+++ 26_vueJS/montyhall/src/App.vue
@@ -1,16 +1,41 @@
 <template>
     <div id="app">
         <h1>Problema de Monty Hall</h1>
-        <TodoGift />
+        <div class="form">
+            <div v-if="!started">
+                <label for="portsAmount">Quantas portas? </label>
+                <input type="text" id="portsAmount" size="3"
+                    v-model.number="portsAmount">
+            </div>
+            <div v-if="!started">
+                <label for="selectedPort">Qual porta é premiada? </label>
+                <input type="text" id="selectedPort" size="3"
+                    v-model.number="selectedPort">
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in portsAmount" :key="i">
+                <TodoDoor :hasGift="i === selectedPort" :number="i" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import TodoGift from "./components/TodoGift.vue"
+import TodoDoor from "./components/TodoDoor.vue"
 
 export default {
     name: 'App',
-    components: { TodoGift }
+    components: { TodoDoor },
+    data: function() {
+        return {
+            started: false,
+            portsAmount: 3,
+            selectedPort: null
+        }
+    }
 }
 </script>
 
@@ -36,5 +61,25 @@ body {
     background-color: #0004;
     padding: 20px;
     margin-bottom: 60px;
+}
+
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+.form, .form input, .form button {
+    margin-bottom: 10px;
+    font-size: 2rem;
+}
+
+.doors {
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
 }
 </style>
