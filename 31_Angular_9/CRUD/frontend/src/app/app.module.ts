c784c52f3f806991ad9eec5a8a939dc4626e1043
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-08-08 17:43:37 -0300

    atualização

--- 31_Angular_9/CRUD/frontend/src/app/app.module.ts
+++ 31_Angular_9/CRUD/frontend/src/app/app.module.ts
@@ -15,7 +15,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { RedDirective } from './directves/red.directive';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { RedDirective } from './directves/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
