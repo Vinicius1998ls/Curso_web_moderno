e5424ff80b0da08e90dcd5e22a8ba64959387abe
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-08-11 14:03:11 -0300

    Fim seção 31 Angular

--- 31_Angular_9/CRUD/frontend/src/app/components/template/header/header.component.ts
+++ 31_Angular_9/CRUD/frontend/src/app/components/template/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HeaderService } from './header.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
   
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  constructor(private HeaderService: HeaderService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  get title(): string {
+    return this.HeaderService.headerData.title
+  }
+  
+  get icon(): string {
+    return this.HeaderService.headerData.icon
+  }
+
+  get routeUrl(): string {
+    return this.HeaderService.headerData.routeUrl
+  }
 }
