197ed071eb19c2dd7108513ae129baff88de7b04
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-09-20 22:08:23 -0300

    Atualização

--- 35_base_de_conhecimento/versao-inicial/frontend/src/App.vue
+++ 35_base_de_conhecimento/versao-inicial/frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-	<div id="app">
-		<Header />
+	<div id="app" :class="{'hide-menu': !isMenuVisible}">
+		<Header title="Cod3r - Base de Conhecimento" :hideToggle="false" />
 		<Menu />
 		<Content />
 		<Footer />
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Header from "./components/template/Header.vue"
 import Menu from "@/components/template/Menu.vue"
 import Content from "@/components/template/Content.vue"
@@ -15,7 +16,8 @@ import Footer from "@/components/template/Footer.vue"
 
 export default {
 	name: "App",
-	Components: { Header, Menu, Content, Footer }
+	components: { Header, Menu, Content, Footer },
+	computed: mapState(['isMenuVisible'])
 }
 </script>
 
@@ -37,8 +39,15 @@ export default {
 		grid-template-rows: 60px 1fr 40px;
 		grid-template-columns: 300px 1fr;
 		grid-template-areas: 
-			"header-header"
+			"header header"
 			"menu content"
 			"menu footer";
+	}
+
+	#app.hide-menu {
+		grid-template-areas: 
+			"header header"
+			"content content"
+			"footer footer";
 	}
 </style>
