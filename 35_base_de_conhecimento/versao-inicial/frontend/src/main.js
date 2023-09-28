e64a293976743806b31d758b5ed672ec8853f9ab
Vinicius1998ls <viniciusdelacerdaschuster@gmail.com>
2023-09-27 21:34:32 -0300

    Atualização

--- 35_base_de_conhecimento/versao-inicial/frontend/src/main.js
+++ 35_base_de_conhecimento/versao-inicial/frontend/src/main.js
@@ -10,9 +10,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-// temporario
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlZpbmljaXVzIFMiLCJlbWFpbCI6IlZpbmljaXVzQHRlc3RlLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2OTU2NDUyNjMsImV4cCI6MTY5NTkwNDQ2M30.LoQMZ5v7BevCGTmCDp6auDrcQ8pKbRh-iCwsQzFGf-A'
-
 new Vue({
   store,
   router,
